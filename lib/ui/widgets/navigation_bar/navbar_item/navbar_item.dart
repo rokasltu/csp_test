ee03b8922364583fd41b218646e8d49ab5046fdc
rokasltu <rokasltu@users.noreply.github.com>
2020-12-05 09:52:29 +0200

    C1 - layout according to docs.

--- lib/ui/widgets/navigation_bar/navbar_item/navbar_item.dart
+++ lib/ui/widgets/navigation_bar/navbar_item/navbar_item.dart
@@ -4,6 +4,7 @@ import 'package:modern_it_homepage/locator.dart';
 import 'package:modern_it_homepage/services/navigation_service.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:modern_it_homepage/extensions/hover_extensions.dart';
 
 import 'navbar_item_desktop.dart';
 import 'navbar_item_mobile.dart';
@@ -32,7 +33,7 @@ class NavBarItem extends StatelessWidget {
         child: ScreenTypeLayout(
           tablet: NavBarItemTabletDesktop(),
           mobile: NavBarItemMobile(),
-        ),
+        ).showCursorOnHover,
       ),
     );
   }
