a8a32d578c7c7c9c1be872e53c356d7226fec0a5
asas1313 <Sriuba11>
2021-03-03 12:53:09 +0200

    improoved folder naming

--- lib/controllers/contactus_controller.dart
+++ lib/controllers/contactus_controller.dart
@@ -1,12 +1,12 @@
 import 'package:get/get.dart';
 
 class ContactusController extends GetxController {
-  final _to = ''.obs;
+  final _from = ''.obs;
   final _messageText = ''.obs;
 
-  get to => _to.value;
+  get from => _from.value;
   get message => _messageText.value;
 
-  set to(String value) => _to.value = value;
+  set from(String value) => _from.value = value;
   set message(String value) => _messageText.value = value;
 }
--- lib/ui/styles/logo.dart
+++ lib/ui/styles/logo.dart
@@ -1,3 +1,3 @@
-const String logo_path = "images/logo.png";
-const String background_img_path = 'images/office-865091_1920.jpg';
+const String logo_path = "resources/images/logo.png";
+const String background_img_path = 'resources/images/office-865091_1920.jpg';
 const double navbar_logo_width = 300;
